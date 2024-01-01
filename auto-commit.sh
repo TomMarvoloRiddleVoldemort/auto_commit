dffb2ea3d840ca259f3418f92d79a6fed62d7f2a
TomMarvoloRiddleVoldemort <fertilizernagar55@gmail.com>
2024-01-02 00:00:00 +0530

    Fake commit for contribution graph

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 
-#Start_date
-  start_date="2024-04-10"
-current_date=$start_date
-last_date=$start_date
+# Configuration
+repo_path="/home/voldemorte/auto_commit"
+start_date="2024-01-01"
+num_commits=50
+commit_message="Fake commit for contribution graph"
 
-#Loop for 250 days
-for ((i=1; i<=30; i++)); do
-	commit_day=$((RANDOM % 10))
-	if [ $commit_day -lt 9 ]; then
-		num_commits=1
-		if [ $((RANDOM %5)) -eq 0 ];then
-			num_commits=$((RANDOM % 3 + 2))
-		fi
-		
-		for ((c=1; c<=$num_commits; c++ )); do
-			num_tasks=$((RANDOM % 5 + 1))
-			indices=($(seq 0 4 | shuf))
+# Navigate to the repository
+cd $repo_path
 
-			for (( j=0; j<num_tasks; j++)); do
-				eval "${tasks[${indices[$j]}]}"
-			done
-			
-			git add .
-			git commit -m "update for $current_date"
-
-			git commit --amend --no-edit --date="$current+date 14:00:00"
-		done
-	fi
-	last_date=$current_date
-	current_Date=$(date -I -d "$current_Date +1 day")
+# Loop to make fake commits
+for i in $(seq 1 $num_commits); do
+  commit_date=$(date -d "$start_date +$i days" "+%a %b %d %H:%M:%S %Y")
+  git add .
+  git commit --date="$commit_date" -m "$commit_message"
 done
+
+# Push commits to GitHub
+git push origin master
